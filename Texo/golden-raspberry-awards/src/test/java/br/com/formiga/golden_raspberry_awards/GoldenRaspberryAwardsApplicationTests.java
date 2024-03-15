073c4b3b951375bbca6466e430ad3fbb462c4a6a
Formiga <marcelomformiga@gmail.com>
2024-03-15 18:43:31 -0300

    Configuracao inicial.

---
+++ Texo/golden-raspberry-awards/src/test/java/br/com/formiga/golden_raspberry_awards/GoldenRaspberryAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.formiga.golden_raspberry_awards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
